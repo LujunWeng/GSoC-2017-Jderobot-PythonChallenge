4563c07a46e9b17b1b6c00e829f2339670965349
LujunW <LujunWeng@outlook.com>
2017-03-30 15:10:00 +1030

    Implement control and info display

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+from enum import Enum
 from modules.conway import Universe
 
 
@@ -6,11 +8,66 @@ def print_universe(u):
     for i in range(h):
         print(u.space[i])
 
-if __name__ == "__main__":
-    u = Universe((10, 10))
-    u.random_reset(50)
-    print_universe(u)
-    while input() != 'q':
-        u.next_generation()
-        print_universe(u)
+class GameStatus(Enum):
+    PAUSE = 0
+    ONGOING = 1
 
+class GameOfLife(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+
+        self.pack()
+        self.canvas = tk.Canvas(self, width=800, height=800)
+        self.canvas.pack(side=tk.BOTTOM)
+        self.control_info = tk.Frame(self)
+        self.control_info.pack(side=tk.TOP, fill=tk.X)
+        self.start_button = tk.Button(self.control_info, text='Start', command=self.start)
+        self.start_button.pack(side=tk.RIGHT)
+        self.pause_button = tk.Button(self.control_info, text='Pause', command=self.pause)
+        self.pause_button.pack(side=tk.RIGHT)
+        self.reset_button = tk.Button(self.control_info, text='Reset & Change Pattern', command=self.reset)
+        self.reset_button.pack(side=tk.RIGHT)
+        self.generation_text_var = tk.StringVar()
+        self.generation_text = tk.Label(self.control_info, textvariable=self.generation_text_var)
+        self.generation_text.pack()
+
+        # Read config file
+        self.u = Universe((30, 30))
+        self.nseed = 300
+
+        self.status = GameStatus.ONGOING
+        self.reset()
+
+    def draw(self):
+        if self.status == GameStatus.ONGOING:
+            self.canvas.delete('all')
+            ds = 30
+            h, w = self.u.dim
+            for i in range(h):
+                for j in range(w):
+                    if self.u.space[i][j] == 1:
+                        self.canvas.create_rectangle(
+                            j*ds, i*ds, j*ds+ds, i*ds+ds, fill="black")
+                    else:
+                        self.canvas.create_rectangle(
+                            j*ds, i*ds, j*ds+ds, i*ds+ds)
+            self.generation_text_var.set("Generation: {:d}".format(self.u.generation))
+            self.u.next_generation()
+            self.after(1000, self.draw)
+
+    def start(self):
+        self.status = GameStatus.ONGOING
+        self.draw()
+
+    def reset(self):
+        self.u.random_reset(self.nseed)
+        self.status = GameStatus.ONGOING
+        self.draw()
+        self.status = GameStatus.PAUSE
+
+    def pause(self):
+        self.status = GameStatus.PAUSE
+
+root = tk.Tk()
+app = GameOfLife(master=root)
+app.mainloop()
